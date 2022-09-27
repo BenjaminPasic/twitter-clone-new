1540761dfa0137c7aafc50af6a965aae0134e08e
Benjamin Pasic <benjamin.pasic@stu.ibu.edu.ba>
2022-09-27 21:40:51 +0200

    added character length limit for posts and progress bar

--- client/src/components/StatusUpdate.js
+++ client/src/components/StatusUpdate.js
@@ -1,13 +1,43 @@
 import "../css/StatusUpdate.css";
+import React, { useEffect, useState, useRef } from "react";
 import Button from "@mui/material/Button";
+import { CircularProgress } from "@mui/material";
 
 export default function StatusUpdate() {
+  const [status, setStatus] = useState("");
+  const [statusCharCount, setStatusCharCount] = useState(0);
+
+  const handleInput = (e) => {
+    if (e.target.textContent.length < 180 || e.key === "Backspace") {
+      setStatus(e.target.textContent);
+      setStatusCharCount(e.target.textContent.length);
+    } else {
+      e.preventDefault();
+    }
+  };
+
   return (
     <div className="status-update">
-      <textarea type="text" placeholder="What's on your mind?" />
-      <Button variant="contained" type="submit" className="button">
-        Post
-      </Button>
+      <div
+        contentEditable="true"
+        className="status-input"
+        onKeyDown={handleInput}
+      ></div>
+      <div className="second-row">
+        <CircularProgress
+          size={25}
+          variant="determinate"
+          value={statusCharCount * 0.555}
+        />
+        <Button
+          variant="contained"
+          type="submit"
+          className="button"
+          margin="normal"
+        >
+          Post
+        </Button>
+      </div>
     </div>
   );
 }
