3779c31f7633b87f6b9617c4ce5b80ed0b953ae7
Benjamin Pasic <benjamin.pasic@stu.ibu.edu.ba>
2023-03-28 23:17:38 +0200

    a

--- server/config/dbConnection.js
+++ server/config/dbConnection.js
@@ -12,7 +12,9 @@ require("dotenv").config({ path: "../.env" });
 //   }
 // );
 
-const dbConnection = new Sequelize(process.env.DATABASE_URL);
+const dbConnection = new Sequelize(
+  "mysql://root:spIFuDm9H4kOAuHrbr9z@containers-us-west-161.railway.app:6662/railway"
+);
 
 dbConnection
   .authenticate()
